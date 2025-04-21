270e5c67e7a6d26fffa09155bbcc32ff6ab5db64
Talonmom-100 <lil_chanti50@hotmail.com>
2025-04-21 10:40:29 -0600

    A NEW LAYER!!!
    YES YES YES YES-

--- js/layers.js
+++ js/layers.js
@@ -132,3 +132,39 @@ milestones: {
     }
 }
 })
+addLayer("t", {
+    name: "Talon Link",
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: false,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+
+    color: "#d2b48c",                       // The color for this layer, which affects many elements.
+    resource: "Talon Links",            // The name of this layer's main prestige resource.
+    row: 1,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "Baby Talon Links",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.b.points },  // A function to return the current amount of baseResource.
+
+    requires: new Decimal(10000),              // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+
+    gainMult() { 
+        tmult = new Decimal(1)                           // Returns your multiplier to your gain of the prestige resource.
+        return tmult               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+
+    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+
+    upgrades: {
+        
+    },
+})
